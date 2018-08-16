e8222ec946a8e37bfcdcf7a8186cd7480e5ccee2
Yussef Lobato Toutenge <yus.toutenge@gmail.com>
2018-08-16 16:33:09 -0300

    Validação de espaçamentos e quebra de linha

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const{app, BrowserWindow, ipcMain} = electron
 
 let win;
 
+var linguagem = new Array("String", "int");
+var codigo;
+var frases = new  Array();
+var palavra = new Array();
+
 function createWindow (){
 
         //Cria a janela
@@ -16,7 +21,26 @@ function createWindow (){
 
 
 ipcMain.on('input:add', function(e, input){
-        win.webContents.send('input:add', input);
+       codigo = input
+       validateLanguage();
 })
 
 app.on('ready', createWindow)
+
+function validateLanguage(){
+
+        var teste
+        if (codigo.indexOf(";")) {
+                frases = codigo.split(";")
+                frases = frases.filter(String)
+
+                // frases.forEach(frase => {
+                //         palavra = frase.split(" ")
+                // });
+                for (var i = 0; i < frases.length; i++) {
+                        palavra[i] = frases[i].split(" ").filter(String)
+                }
+        }
+        
+         win.webContents.send('input:add', palavra);
+}
