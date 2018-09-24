a6ac1d47a7bf494667a93ef9f6627bd5143de2c5
Yussef Toutenge <yus.toutenge@gmail.com>
2018-09-24 19:28:09 -0300

    Lexer completo até validação com filipe

--- lexer.js
+++ lexer.js
@@ -8,23 +8,40 @@ module.exports = {
         var i = 0
 
         var isOperator = function(c){
-            return /[+\-*\/\^%=(),]/.test(c)
+            return /[+\-*\/\^%=()<>,!]/.test(c)
         }
     
         var isDigit = function(c){
             return /[0-9]/.test(c)
         }
     
+        var isPontoV = function(c){
+            return /;/.test(c)
+        }
+
         var isWhiteSpace = function(c){
             return /[\s]/.test(c)
         }
 
         var isString = function(c){
-            return typeof c == "string" && !isOperator(c) && !isWhiteSpace(c) && !isDigit(c)
+            return typeof c == "string" && !isOperator(c) && !isWhiteSpace(c) && !isDigit(c) && !isPontoV(c)
         }
 
         var advance = function () { 
             return c = input[++i] 
+        }
+
+        var getUndefined = function (c){
+            idn = c
+            operador: do{
+                advance()
+                if(c === undefined || isWhiteSpace(c)){
+                    break operador
+                }
+                idn += c  
+            }while (!isWhiteSpace(c)) 
+
+            return idn
         }
 
         var addToken = function (type, value) {
@@ -40,17 +57,44 @@ module.exports = {
             if (isWhiteSpace(c)){
                 advance()
             } else if (isOperator(c)) {
-                if(c == "+"){ 
-                    addToken("mais",c)
+                idn = c
+                operador: do{
                     advance()
-                } else if(c == "-"){ 
-                    addToken("menos",c)
+                    if(c === undefined || isWhiteSpace(c)){
+                        break operador
+                    }
+                    idn += c  
+                }while (!isWhiteSpace(c)) 
+
+
+                if(idn == "+"){ 
+                    addToken("mais",idn)
                     advance()
-                } else if(c == "*"){ 
-                    addToken("vezes",c)
+                } else if(idn == "-"){ 
+                    addToken("menos",idn)
                     advance()
-                } else if(c == "/"){ 
-                    addToken("divisao",c)
+                } else if(idn == "*"){ 
+                    addToken("vezes",idn)
+                    advance()
+                } else if(idn == "/"){ 
+                    addToken("divisao",idn)
+                    advance()
+                } else if(idn == "=="){ 
+                    addToken("comparacao",idn)
+                    advance()
+                } else if(idn == "!="){ 
+                    addToken("diferenca",idn)
+                    advance()
+                } else if(idn == "="){ 
+                    addToken("igual",idn)
+                    advance()
+                } else if(idn == "++"){ 
+                    addToken("adicionar1",idn)
+                    advance()
+                } else if(idn == "--"){ 
+                    addToken("subtrair1",idn)
+                    advance()
+                } else {
                     advance()
                 }
                 
@@ -74,15 +118,14 @@ module.exports = {
                 addToken("number", num)
 
             } else if (isString(c)) {
-                var idn = c
-
-                while (!isWhiteSpace(advance()) || !isOperator(advance())){
-                    idn += c
-                    if(input.substring(input.length -1) == c){
-                        c == " "
-                        break;
+                idn = c
+                outerloop: do{
+                    advance()
+                    if(c === undefined || isWhiteSpace(c)){
+                        break outerloop
                     }
-                }
+                    idn += c  
+                }while (!isWhiteSpace(c) || !isOperator(c))       
 
                 if(idn == "int"){ 
                     addToken("integer", idn)
@@ -92,6 +135,30 @@ module.exports = {
                     addToken("float", idn)
                 } else if(idn == "bool"){
                     addToken("bool", idn)
+                } else if(idn == "func"){
+                    addToken("funcao", idn)
+                } else if(idn == "if"){
+                    addToken("if", idn)
+                } else if(idn == "else"){
+                    addToken("else", idn)
+                } else if(idn == "for"){
+                    addToken("for", idn)
+                } else if(idn == "while"){
+                    addToken("while", idn)
+                } else if(idn == "jamal"){
+                    addToken("jamal", idn)
+                } else if(idn == "print"){
+                    addToken("print", idn)
+                } else if(idn == "println"){
+                    addToken("println", idn)
+                } else if(idn == "read"){
+                    addToken("read", idn)
+                } else if(idn == "readln"){
+                    addToken("readln", idn)
+                } else if(idn == "close"){
+                    addToken("fechar", idn)
+                } else if(idn == "pause"){
+                    addToken("pausar", idn)
                 } else {
                     addToken("identifier", idn)
                 }
